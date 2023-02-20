1e4570188d2212dcb814a870c645d0eac28baea8
metacode22 <alohajune22@gmail.com>
2023-02-20 21:40:36 +0900

    chore: react query client 및 devtools 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
