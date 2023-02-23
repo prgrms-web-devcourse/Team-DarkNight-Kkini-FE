f90782f4237522782e5b82a1e9c661a0ff3b555b
suhwa <hansuhwa2005@gmail.com>
2023-02-23 17:56:41 +0900

    chore: 필요한 디펜던시 설치(chakra, hook-form, icons, kakaoMaps)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
@@ -7,8 +8,10 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Component {...pageProps} />
+      <ChakraProvider resetCSS>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Component {...pageProps} />
+      </ChakraProvider>
     </QueryClientProvider>
   );
 }
