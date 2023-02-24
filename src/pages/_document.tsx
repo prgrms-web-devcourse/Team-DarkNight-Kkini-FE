c1e6f73a4b1a426b0e138a42a2f4c5756b573cae
suhwa <hansuhwa2005@gmail.com>
2023-02-24 14:28:26 +0900

    chore: kakao, google api key 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,28 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
-export default function Document() {
+const Document = () => {
   return (
     <Html lang='ko'>
       <Head />
       <body>
         <Main />
         <NextScript />
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${
+            process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY as string
+          }&libraries=services,clusterer&autoload=false`}
+          strategy='beforeInteractive'
+        />
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${
+            process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY as string
+          }&libraries=services,clusterer&autoload=false`}
+          strategy='beforeInteractive'
+        />
       </body>
     </Html>
   );
-}
+};
+
+export default Document;
