2a99e03b2a4c53258c947e594445643f40910f1f
sdwoo <damjang67@naver.com>
2023-02-26 01:17:27 +0900

    feat: 버튼 공통 컴포넌트 구현

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,36 @@
+import { Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+import React, { ReactNode } from 'react';
+
+type Props = {
+  width?: string | number;
+  height?: string | number;
+  children?: ReactNode;
+};
+
+const Button = ({ width = '100%', height = '3rem', ...props }: Props) => {
+  return (
+    <ButtonContainer
+      as='button'
+      align='center'
+      justify='center'
+      gap='8px'
+      w={width}
+      h={height}
+      color='white'
+      borderRadius='8px'
+      {...props}>
+      {props.children}
+    </ButtonContainer>
+  );
+};
+
+const ButtonContainer = styled(Flex)`
+  background-color: ${({ theme }) => theme.color.primary};
+
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
+
+export default Button;
