03c9639888db24f02bb2b0f3591e8c22054e9403
metacode22 <alohajune22@gmail.com>
2023-02-26 00:49:37 +0900

    design: 랜덤 맛집 뽑기 버튼 위치 이동

--- src/components/common/Buttons/RecommendRandomRestaurantButton.tsx
+++ src/components/common/Buttons/RecommendRandomRestaurantButton.tsx
@@ -8,12 +8,17 @@ const RecommendRandomRestaurantButton = () => {
 export default RecommendRandomRestaurantButton;
 
 const StyledButton = styled(Button)`
-  padding: 1.5rem 4rem;
+  position: absolute;
+  bottom: 1.5rem;
+  left: 50%;
+  z-index: 10;
+  padding: 1.5rem 5rem;
   color: white;
   background-color: ${({ theme }) => theme.color.primary};
   box-shadow: ${({ theme }) => theme.boxShadow.button};
+  transform: translateX(-50%);
 
   &:hover {
-    background-color: black;
+    background-color: ${({ theme }) => theme.color.primary};
   }
 `;
