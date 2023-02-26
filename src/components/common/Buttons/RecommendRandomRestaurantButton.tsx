ec62ac124f5baf30d4e692bd2667d34d609589cb
metacode22 <alohajune22@gmail.com>
2023-02-26 13:55:25 +0900

    refactor: 랜덤 맛집 뽑기 버튼의 스타일 코드를 chakra에게 위임

--- src/components/common/Buttons/RecommendRandomRestaurantButton.tsx
+++ src/components/common/Buttons/RecommendRandomRestaurantButton.tsx
@@ -2,29 +2,42 @@ import { Button } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 type RecommendRandomRestaurantButtonProps = {
+  isLoading: boolean;
   onClick: () => void;
 };
 
 const RecommendRandomRestaurantButton = ({
+  isLoading,
   onClick,
 }: RecommendRandomRestaurantButtonProps) => {
-  return <StyledButton onClick={onClick}>랜덤 맛집 뽑기</StyledButton>;
+  return (
+    <StyledButton
+      position='absolute'
+      bottom='1.5rem'
+      left='50%'
+      zIndex={10}
+      padding='1.5rem 5rem'
+      color='white'
+      transform='translateX(-50%)'
+      isLoading={isLoading}
+      onClick={onClick}>
+      랜덤 맛집 뽑기
+    </StyledButton>
+  );
 };
 
 export default RecommendRandomRestaurantButton;
 
 const StyledButton = styled(Button)`
-  position: absolute;
-  bottom: 1.5rem;
-  left: 50%;
-  z-index: 10;
-  padding: 1.5rem 5rem;
-  color: white;
   background-color: ${({ theme }) => theme.color.primary};
   box-shadow: ${({ theme }) => theme.boxShadow.button};
-  transform: translateX(-50%);
 
-  &:hover {
+  &:hover,
+  &:disabled {
     background-color: ${({ theme }) => theme.color.primary};
+
+    &:hover {
+      background-color: ${({ theme }) => theme.color.primary};
+    }
   }
 `;
