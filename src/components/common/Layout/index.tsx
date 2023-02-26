4a937959849659615ac1624cd551513363e3ded1
suhwa <hansuhwa2005@gmail.com>
2023-02-26 16:04:08 +0900

    chore: next/font/local 이용해 폰트파일 추가 & 설정

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -2,11 +2,12 @@ import { Box, ChakraProvider, Flex } from '@chakra-ui/react';
 import Header from 'components/common/Layout/Header';
 import Navigation from 'components/common/Layout/Navigation';
 import { ReactNode } from 'react';
+import { BaseFont } from 'styles/fonts';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <ChakraProvider resetCSS>
-      <Flex flexDirection='column' id='app'>
+      <Flex flexDirection='column' id='app' className={BaseFont.className}>
         <Header />
         <Box as='main' flex={1}>
           {children}
---
+++ src/styles/fonts.ts
@@ -0,0 +1,9 @@
+import localFont from '@next/font/local';
+
+export const BaseFont = localFont({
+  src: '../../public/fonts/Pretendard-Regular.woff',
+});
+
+export const GangwonEduFont = localFont({
+  src: '../../public/fonts/GangwonEdu_OTFBoldA.woff',
+});
