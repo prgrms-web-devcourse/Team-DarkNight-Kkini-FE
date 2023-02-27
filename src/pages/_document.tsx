69df40238b4fe3946548c2bc3da2d23e3eaf830e
metacode22 <alohajune22@gmail.com>
2023-02-28 00:48:35 +0900

    fix: google maps api 스크립트 src 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,8 +13,7 @@ const Document = () => {
           strategy='beforeInteractive'
         />
         <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}&libraries=places'
-          `}
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}&libraries=places`}
           strategy='beforeInteractive'
         />
       </body>
