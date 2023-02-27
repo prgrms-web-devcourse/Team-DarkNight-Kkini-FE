b92746254e5afb66532024938156dc1eca14df32
metacode22 <alohajune22@gmail.com>
2023-02-28 00:47:49 +0900

    feat: 랜덤 맛집 추천 기능에서 google api 연결

--- src/hooks/kakaoMap/useRecommendRandomRestaurant.ts
+++ src/hooks/kakaoMap/useRecommendRandomRestaurant.ts
@@ -33,20 +33,23 @@ const useRecommendRandomRestaurant = () => {
     const nearbyRestaurants: kakao.maps.services.PlacesSearchResultItem[] = [];
     kakaoPlacesService.categorySearch(
       KAKAO_RESTAURANT_CATEGORY_CODE,
-      (result, status, pagination) => {
+      (resultByKakaoCategorySearch, status, pagination) => {
         // To Do
         // ZERO_RESULT, ERROR 각 상황 처리 필요
         const { OK, ZERO_RESULT, ERROR } = kakao.maps.services.Status;
 
         if (status === OK) {
-          nearbyRestaurants.push(...result);
+          nearbyRestaurants.push(...resultByKakaoCategorySearch);
           pagination.hasNextPage && pagination.nextPage();
         }
 
         // 더 이상 맛집 데이터가 없을 때 랜덤으로 맛집을 추천하는 로직을 수행.
         // 최대 45개의 맛집 데이터를 얻을 수 있다.
         if (!pagination.hasNextPage) {
+          // 랜덤 숫자 생성
           const randomIndex = Math.floor(Math.random() * nearbyRestaurants.length);
+
+          // kakao marker 생성
           const markerImage = new kakao.maps.MarkerImage(
             RESTAURANT_BADGE_IMAGE_FILE_PATH,
             new kakao.maps.Size(DEFAULT_BADGE_IMAGE_SIZE, DEFAULT_BADGE_IMAGE_SIZE)
@@ -59,7 +62,40 @@ const useRecommendRandomRestaurant = () => {
             image: markerImage,
           });
 
-          // const googlePlacesService = new google
+          // google api
+          const dummyGoogleMap = new google.maps.Map(document.createElement('div'), {
+            center: {
+              lat: currentLatitude,
+              lng: currentLongitude,
+            },
+            zoom: 15,
+          });
+
+          const googlePlacesService = new google.maps.places.PlacesService(
+            dummyGoogleMap
+          );
+
+          // google textSearch
+          googlePlacesService.textSearch(
+            {
+              query: nearbyRestaurants[randomIndex].road_address_name,
+              type: 'restaurant',
+            },
+            (resultByGoogleTextSearch, status, pagination) => {
+              if (!resultByGoogleTextSearch || !resultByGoogleTextSearch[0].place_id)
+                return;
+
+              // google getDetails
+              googlePlacesService.getDetails(
+                {
+                  placeId: resultByGoogleTextSearch[0].place_id,
+                },
+                (resultByGoogleGetDetails, status) => {
+                  if (!resultByGoogleGetDetails) return;
+                }
+              );
+            }
+          );
 
           setKakaoMapOptions((previousKakaoMapOptions) => ({
             ...previousKakaoMapOptions,
