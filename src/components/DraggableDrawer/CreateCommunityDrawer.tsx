fa435526c8703fe914b92f2cdbe5d4561e8abfa1
sdwoo <damjang67@naver.com>
2023-03-01 03:35:04 +0900

    feat: CreateCommunityDrawer 컴포넌트 구현

--- src/components/DraggableDrawer/CreateCommunityDrawer.tsx
+++ src/components/DraggableDrawer/CreateCommunityDrawer.tsx
@@ -1,10 +1,16 @@
 import { Box } from '@chakra-ui/react';
 
+import SearchRestaurant from '../SearchRestaurant/index';
+import CreateCommunityContent from './CreateCommunityContent';
 import DraggableDrawer from './index';
+
 const CreateCommunityDrawer = () => {
   return (
     <DraggableDrawer>
-      <Box h='100%'>Test</Box>
+      <Box h='100%'>
+        <SearchRestaurant />
+        <CreateCommunityContent />
+      </Box>
     </DraggableDrawer>
   );
 };
