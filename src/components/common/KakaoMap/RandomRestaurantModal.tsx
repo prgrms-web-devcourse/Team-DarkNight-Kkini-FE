0606db7fe6cebd5ac31421756d69cf608ddf2f69
metacode22 <alohajune22@gmail.com>
2023-02-28 17:57:22 +0900

    feat: RandomRestaurantModal에서 randomRestaurant 객체의 값에 따라 마크업이 변경되도록 구현

--- src/components/common/KakaoMap/RandomRestaurantModal.tsx
+++ src/components/common/KakaoMap/RandomRestaurantModal.tsx
@@ -27,15 +27,17 @@ const RandomRestaurantModal = forwardRef<HTMLDivElement, RandomRestaurantModalPr
         borderRadius='0.5rem 0.5rem 0 0'
         backgroundColor='white'>
         {/* To Do: 가게 제목 누르면 해당 가게의 카카오 place url로 이동하게 만들자. by 승준 */}
+        {/* To Do: 스타일링 다듬을 필요 있음. by 승준 */}
+        {/* To Do: randomRestaurant의 속성들이 undefined일 경우가 많으니, undefined이면 안 보이게 처리 필요. by 승준 */}
         <Flex justifyContent='space-between'>
           <Stack direction='row' paddingBottom='0.5rem'>
             {/* To Do: 음식점 카테고리는 거르자. by 승준 */}
-            <Category>음식점</Category>
-            <Category>술집</Category>
-            <Category>치킨</Category>
+            {randomRestaurant.categories?.map((category) => (
+              <Category key={category}>{category}</Category>
+            ))}
           </Stack>
           <Stack direction='row' paddingBottom='0.5rem'>
-            <Category>영업 중</Category>
+            <Category>{randomRestaurant.isOpen ? '영업 중' : '영업 종료'}</Category>
           </Stack>
         </Flex>
         <Flex alignItems='center' justifyContent='space-between'>
@@ -46,7 +48,7 @@ const RandomRestaurantModal = forwardRef<HTMLDivElement, RandomRestaurantModalPr
               _hover={{
                 textDecoration: 'underline',
               }}>
-              가게 제목
+              {randomRestaurant.placeName}
             </Heading>
             <BiRightArrowCircle size='1.1rem' cursor='pointer' />
           </Flex>
@@ -55,8 +57,8 @@ const RandomRestaurantModal = forwardRef<HTMLDivElement, RandomRestaurantModalPr
           </Box>
         </Flex>
         {/* To Do: 별점 by 승준 */}
-        <Box>주소</Box>
-        <Box>전화번호</Box>
+        <Box>{randomRestaurant.roadAddressName}</Box>
+        <Box>{randomRestaurant.phoneNumber}</Box>
         {/* To Do: 넓이/높이(aspect ratio)랑 스크롤바 스타일링 필요 by 승준 */}
         <Flex overflowX='auto' gap='1rem'>
           {randomRestaurant.photoUrls?.map((photoUrl) => (
