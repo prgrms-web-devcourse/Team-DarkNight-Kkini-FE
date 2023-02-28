432a543de2b2c87f191673596f631e7c89bfb1ca
sdwoo <damjang67@naver.com>
2023-03-01 03:30:51 +0900

    feat: 맛모임 가게 검색 헤더 추가

--- src/components/DraggableDrawer/DrawerHeader.tsx
+++ src/components/DraggableDrawer/DrawerHeader.tsx
@@ -1,15 +1,18 @@
-import { Box } from '@chakra-ui/react';
-
+import { Box, Text } from '@chakra-ui/react';
+import { GangwonEduFont } from 'styles/fonts';
 const DrawerHeader = () => {
   return (
     <Box
       position='relative'
-      h={24}
+      h={16}
       borderTopLeftRadius={8}
       borderTopRightRadius={8}
       pt={4}
       pb={4}>
-      <Box w={32} h={2} borderRadius={2} bg='#d0d0d0' margin='auto'></Box>
+      <Box w={16} h={2} borderRadius={2} bg='#d0d0d0' margin='auto'></Box>
+      <Text fontSize='lg' className={GangwonEduFont.className} pl={5}>
+        맛모임 가게 검색
+      </Text>
     </Box>
   );
 };
