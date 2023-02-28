f305720b80aa5c175361fc338daf3d84dca54805
metacode22 <alohajune22@gmail.com>
2023-02-28 15:38:48 +0900

    refactor: RandomRestaurantType를 types 폴더에서 관리

--- src/types/kakaoMap.ts
+++ src/types/kakaoMap.ts
@@ -5,3 +5,15 @@ export type KakaoMapOptions = {
   };
   level: number;
 };
+
+export type RandomRestaurantType = {
+  placeName?: string;
+  categories?: string[];
+  roadAddressName?: string;
+  rating?: number;
+  photoUrls?: string[];
+  isOpen?: boolean;
+  kakaoPlaceUrl?: string;
+  phoneNumber?: string;
+  marker?: kakao.maps.Marker | null;
+};
