5f49c085c37f91a67f32d029532e12f023e1c0d6
sdwoo <damjang67@naver.com>
2023-03-01 20:12:03 +0900

    refactor: RestaurantProps 타입명 변경

--- src/types/restaurant.ts
+++ src/types/restaurant.ts
@@ -1,4 +1,4 @@
-export type RestaurantProps = {
+export type RestaurantFromKeywordSearchProps = {
   category_name: string; // ex. '음식점 > 한식 > 해물,생선 > 장어'
   place_name: string; // ex. '자갈치 곰장어'
   road_address_name: string; // ex. '부산 금정구 오시게로28번길 4'
