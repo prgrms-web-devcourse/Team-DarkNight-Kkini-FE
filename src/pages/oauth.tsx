243a1118f796e59c4f09c14e11b22eb9fe7c331c
suhwa <hansuhwa2005@gmail.com>
2023-03-02 03:34:05 +0900

    feat: 로그인 후처리 작업: 토큰 설정, 홈으로 이동

---
+++ src/pages/oauth.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { isLogin } from 'stores/auth';
+import { setAccessToken } from 'utils/apis/axios';
+
+const OAuthLogin = () => {
+  const router = useRouter();
+  const setLoginState = useSetRecoilState(isLogin);
+
+  useEffect(() => {
+    const token = router.query.accessToken as string;
+    setAccessToken(token);
+    setLoginState(true);
+    router.push('/');
+  }, [router]);
+
+  return <div>로그인 중..</div>;
+};
+
+export default OAuthLogin;
