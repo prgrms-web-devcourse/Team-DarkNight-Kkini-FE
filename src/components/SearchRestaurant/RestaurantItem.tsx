e6680212c9b2c728b70df514bfaec0b3033748ea
sdwoo <damjang67@naver.com>
2023-03-01 17:09:13 +0900

    refactor: Restaurant Item Wrapper 제거

--- src/components/SearchRestaurant/RestaurantItem.tsx
+++ src/components/SearchRestaurant/RestaurantItem.tsx
@@ -18,9 +18,7 @@ const RestaurantItem = ({ name, address, photo }: RestaurantItemProps) => {
       borderRadius='8px'
       p='0.2rem'
       mb='0.2rem'>
-      <Box pos='relative' borderRadius='8px' width='50px' height='50px'>
-        <Image src={'/images/default_restaurant.svg'} alt={name} width={50} height={50} />
-      </Box>
+      <Image src={'/images/default_restaurant.svg'} alt={name} width={50} height={50} />
       <Flex flexDir='column' justify='space-around' pl='0.2rem'>
         <Text fontSize='xl' fontWeight='bold'>
           {name}
