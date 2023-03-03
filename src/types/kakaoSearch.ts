1116ce8e387708101b0f4a58cef11c1b0be15462
sdwoo <damjang67@naver.com>
2023-03-03 22:48:24 +0900

    feat: 밥모임 식당 검색 결과 타입 선언

---
+++ src/types/kakaoSearch.ts
@@ -0,0 +1,30 @@
+type DocumentsType = {
+  collection: string;
+  thumbnail_url: string;
+  image_url: string;
+  width: number;
+  height: number;
+  display_sitename: string;
+  doc_url: string;
+  datetime: string;
+};
+
+export type AxiosPhotoResponseValue = {
+  documents: DocumentsType[];
+  meta: {
+    total_count: number;
+    pageable_count: number;
+    is_end: boolean;
+  };
+};
+
+export type RestaurantFromKeywordSearchProps = {
+  placeName: string; // ex. '자갈치 곰장어' - kakao api
+  categories: string[]; // ex. ['음식점', '한식', '해물,생선', '장어'] - kakao api
+  roadAddressName: string; // ex. '부산 금정구 오시게로28번길 4' - kakao api
+  photoUrls: string[]; // ex. ['https://~', 'https://~', 'https://~', ...] - google api
+  kakaoPlaceUrl: string; // ex. "http://place.map.kakao.com/24199893" - kakao api
+  phoneNumber: string; // 전화번호 ex. '02-017-3495' - kakao api
+  x: number; // ex. 129.0868070386026 - kakao api
+  y: number; // ex. 35.22008482785265 - kakao api
+};
