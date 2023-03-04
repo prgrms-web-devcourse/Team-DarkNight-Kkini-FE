a5ced01cc8e4943c88e8580af2113523832ad9fa
sdwoo <damjang67@naver.com>
2023-03-05 01:57:22 +0900

    chore: 자주 쓰는 background 색 추가

--- src/styles/chakra-theme.ts
+++ src/styles/chakra-theme.ts
@@ -3,6 +3,7 @@ import { extendTheme } from '@chakra-ui/react';
 const customTheme = {
   colors: {
     primary: '#FF5C00',
+    background: '#F9F9F9',
   },
   shadows: {
     button: '2px 2px 2px 1px rgba(0,0,0,0.25)',
