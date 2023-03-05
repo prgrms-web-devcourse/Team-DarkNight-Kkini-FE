dd1ec5f39c5054b3eebf4d9a67d7a8fe8255f6e7
suhwa <hansuhwa2005@gmail.com>
2023-03-06 02:24:24 +0900

    chore: 백엔드 테스트 서버로 엔드포인트 추가

--- env.d.ts
+++ env.d.ts
@@ -5,5 +5,6 @@ declare namespace NodeJS {
 
     readonly NEXT_PUBLIC_OAUTH_REDIRECT_URL: string;
     readonly NEXT_PUBLIC_KAKAO_SEARCH_API_KEY: string;
+    readonly NEXT_PUBLIC_API_END_POINT: string;
   }
 }
