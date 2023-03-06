f3b116afde8d9a6e7c0b1f3e26a5ed729db655d4
suhwa <hansuhwa2005@gmail.com>
2023-03-07 00:43:22 +0900

    feat: header 유저 프로필 UI
    맛에 진심인, nickname 문구 추가

--- src/components/common/UserProfile.tsx
+++ src/components/common/UserProfile.tsx
@@ -1,10 +1,18 @@
-import { Avatar } from '@chakra-ui/react';
+import { Avatar, Flex, Text } from '@chakra-ui/react';
 import { useGetUser } from 'hooks/query/useUser';
 
 const UserProfile = () => {
   const { data } = useGetUser();
 
-  return <Avatar src={data?.profileImgUrl} width='35' height='35' />;
+  return (
+    <Flex alignItems='center'>
+      <Flex fontWeight='600'>
+        <Text color='primary'>맛에 진심인, </Text>
+        <Text marginLeft='0.25rem'>{data?.nickname}</Text>
+      </Flex>
+      <Avatar src={data?.profileImgUrl} width='35' height='35' marginLeft='0.7rem' />
+    </Flex>
+  );
 };
 
 export default UserProfile;
