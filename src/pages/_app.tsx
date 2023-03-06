485cfe1f69c1445594a17a132d83f19fafef0a11
suhwa <hansuhwa2005@gmail.com>
2023-03-06 22:20:09 +0900

    feat: react-query에서 ssr 할 수 있도록 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,36 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  DehydratedState,
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from 'components/common/Layout';
 import { KakaoMapProvider } from 'contexts/kakaoMap';
 import { RandomRestaurantProvider } from 'contexts/kakaoMap/randomRestaurant';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
 
-const queryClient = new QueryClient();
+const App = ({
+  Component,
+  pageProps,
+}: AppProps<{ dehydratedState: DehydratedState }>) => {
+  const [queryClient] = useState(() => new QueryClient());
 
-const App = ({ Component, pageProps }: AppProps) => {
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        <KakaoMapProvider>
-          <RandomRestaurantProvider>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </RandomRestaurantProvider>
-        </KakaoMapProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
+        <Hydrate state={pageProps.dehydratedState}>
+          <KakaoMapProvider>
+            <RandomRestaurantProvider>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </RandomRestaurantProvider>
+          </KakaoMapProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Hydrate>
       </QueryClientProvider>
     </RecoilRoot>
   );
