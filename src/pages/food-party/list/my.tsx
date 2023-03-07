cd3e547fd19a802ee2111163019d80a80ae11d60
metacode22 <alohajune22@gmail.com>
2023-03-07 17:31:08 +0900

    refactor: 내 밥모임 목록 페이지 동적 라우팅 제거

--- src/pages/food-party/list/my.tsx
+++ src/pages/food-party/list/my.tsx
@@ -1,4 +1,5 @@
 import { Flex, Heading } from '@chakra-ui/react';
+import { QueryClient } from '@tanstack/react-query';
 import FoodPartyList from 'components/FoodParty/FoodPartyList';
 import FoodPartyListSkeleton from 'components/FoodParty/FoodPartyListSkeleton';
 import { useGetMyFoodPartyList } from 'hooks/query/useFoodParty';
@@ -10,7 +11,6 @@ type MyFoodPartyListProps = {
 };
 
 const MyFoodPartyList = ({ userId }: MyFoodPartyListProps) => {
-  console.log(userId);
   const router = useRouter();
   // To Do: 현재 인원 실시간 업데이트를 위한 refetch 필요 by 승준
   const { data: myFoodPartyList, isLoading } = useGetMyFoodPartyList(userId);
@@ -30,14 +30,12 @@ const MyFoodPartyList = ({ userId }: MyFoodPartyListProps) => {
 
 export default MyFoodPartyList;
 
-// useRouter를 사용하면 userId가 undefined가 찍힐 수 있음.
-// 따라서 userId를 곧바로 useGetMyFoodPartyList로 넣어주기 위해 getServerSideprops 이용.(undefined가 안되도록)
-// 그래서 따로 await할 로직이 필요 없어 eslint-disable 처리.
-// 하지만 getServerSideProps는 async 해줘야 제대로 처리됨.
 export const getServerSideProps: GetServerSideProps<{ userId: number }> = async ({
   params,
-  // eslint-disable-next-line @typescript-eslint/require-await
 }) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery([]);
+
   return {
     props: {
       userId: Number(params?.userId),
