96926656e24357dbb3a312f8fa0e1fd231fedca0
metacode22 <alohajune22@gmail.com>
2023-03-07 11:58:59 +0900

    feat: 가게로 검색한 밥모임 목록 페이지 틀 작성

---
+++ src/pages/food-party/place/[placeId].tsx
@@ -0,0 +1,16 @@
+import { Flex, Heading } from '@chakra-ui/react';
+import FoodPartyList from 'components/FoodParty/FoodPartyList';
+import { GetServerSidePropsContext } from 'next';
+
+const SearchedFoodPartyList = () => {
+  return (
+    <Flex flexDirection='column' padding='1rem'>
+      <Heading paddingBottom='1rem'>00 맛집의 밥모임 목록</Heading>
+      <FoodPartyList foodPartyList={} onClick={} />
+    </Flex>
+  );
+};
+
+export default SearchedFoodPartyList;
+
+export const getServerSideProps = (context: GetServerSidePropsContext) => {};
