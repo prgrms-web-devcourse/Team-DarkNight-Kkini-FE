312e99ea2cd8d711a66e45dd1fdf3abdf74bd58a
metacode22 <alohajune22@gmail.com>
2023-03-10 00:36:34 +0900

    refactor: FoodPartyDetail의 컨텐츠 부분을 FoodPartyDetailContent로 분리

--- src/pages/food-party/detail/[partyId].tsx
+++ src/pages/food-party/detail/[partyId].tsx
@@ -1,7 +1,8 @@
-import { Button, Flex, Text, useDisclosure } from '@chakra-ui/react';
+import { Flex, useDisclosure } from '@chakra-ui/react';
 import { dehydrate, QueryClient } from '@tanstack/react-query';
 import GoHomeWhenErrorInvoked from 'components/common/GoHomeWhenErrorInvoked';
 import FoodPartyDetailChangeStatusButton from 'components/FoodParty/FoodPartyDetail/FoodPartyDetailChangeStatusButton';
+import FoodPartyDetailContent from 'components/FoodParty/FoodPartyDetail/FoodPartyDetailContent';
 import FoodPartyDetailHeader from 'components/FoodParty/FoodPartyDetail/FoodPartyDetailHeader';
 import FoodPartyDetailSkeleton from 'components/FoodParty/FoodPartyDetail/FoodPartyDetailSkeleton';
 import FoodPartyMemberList from 'components/FoodParty/FoodPartyDetail/FoodPartyMemberList';
@@ -9,11 +10,9 @@ import RestaurantBottomDrawer from 'components/Restaurant/RestaurantBottomDrawer
 import { useGetFoodPartyDetail } from 'hooks/query/useFoodParty';
 import { useGetUser } from 'hooks/query/useUser';
 import { GetServerSideProps } from 'next';
-import { AiOutlineCalendar, AiOutlineClockCircle, AiOutlineSearch } from 'react-icons/ai';
 import { fetchFoodPartyDetail } from 'services/foodParty';
 import { fetchUser } from 'services/user';
 import QUERY_KEYS from 'utils/constants/queryKeys';
-import { templatePromiseDate, templatePromiseTime } from 'utils/helpers/foodParty';
 
 // To Do: 404 처리 by 승준
 // partyId로 조회하는 페이지
@@ -34,9 +33,6 @@ const FoodPartyDetail = ({ partyId }: { partyId: string }) => {
   if (isLoading) return <FoodPartyDetailSkeleton />;
   if (error) return <div>{error.toString()}</div>;
 
-  // To Do: Date 등 따로 컴포넌트를 빼자.
-  const [year, month, day, hour, minute] = foodPartyDetail!.promiseTime;
-
   // To Do: isLeader, isMember, isFull, status에 따라 다르게
   // const router = useRouter();
   // const handleClickButton = () => {
@@ -52,32 +48,16 @@ const FoodPartyDetail = ({ partyId }: { partyId: string }) => {
           flexDirection='column'
           padding='1rem'
           gap='0.5rem'>
-          {/* 헤더 */}
           <FoodPartyDetailHeader
             status={foodPartyDetail.status}
             category={foodPartyDetail.category}
             foodPartyName={foodPartyDetail.name}
           />
-          {/* 데이터 & 가게 정보 */}
-          <Flex flexDirection='column' gap='0.5rem'>
-            <Flex alignItems='center' gap='0.5rem'>
-              <AiOutlineCalendar />
-              <Text>{templatePromiseDate(year, month, day)}</Text>
-            </Flex>
-            <Flex alignItems='center' gap='0.5rem'>
-              <AiOutlineClockCircle />
-              <Text>{templatePromiseTime(hour, minute)}</Text>
-            </Flex>
-            <Flex alignItems='center' gap='0.5rem'>
-              <AiOutlineSearch />
-              <Button onClick={onOpen} height='1.5rem'>
-                맛집 정보
-              </Button>
-            </Flex>
-          </Flex>
-          {/* 내용 */}
-          <Text margin='1rem 0'>{foodPartyDetail.content}</Text>
-          {/* 멤버 리스트 */}
+          <FoodPartyDetailContent
+            promiseTime={foodPartyDetail.promiseTime}
+            content={foodPartyDetail.content}
+            onClick={onOpen}
+          />
           <FoodPartyMemberList
             memberList={foodPartyDetail.members}
             capacity={foodPartyDetail.capacity}
