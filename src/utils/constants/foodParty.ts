22946356b1485847c27c0186b9dd4085640bdbac
sdwoo <damjang67@naver.com>
2023-03-10 12:57:29 +0900

    feat: review 아이템 추가

--- src/utils/constants/foodParty.ts
+++ src/utils/constants/foodParty.ts
@@ -14,4 +14,64 @@ export const foodPartyCategory = [
     icon: '/images/friend.svg',
     bgColor: '#F3F2F9',
   },
+  {
+    title: '매너 있는 자리',
+    icon: '/images/friend.svg',
+    bgColor: '#F3F2F9',
+  },
+];
+
+export const foodPartyLeaderReviewState = [
+  {
+    title: '괴식가',
+    icon: '/images/leader-lowest.svg',
+    score: 1,
+  },
+  {
+    title: '맛알못',
+    icon: '/images/leader-low.svg',
+    score: 2,
+  },
+  {
+    title: '일반인',
+    icon: '/images/leader-mid.svg',
+    score: 3,
+  },
+  {
+    title: '맛잘알',
+    icon: '/images/leader-high.svg',
+    score: 4,
+  },
+  {
+    title: '미식가',
+    icon: '/images/leader-highest.svg',
+    score: 5,
+  },
+];
+export const foodPartyMemberReviewState = [
+  {
+    title: '최악',
+    icon: '/images/member-lowest.svg',
+    score: 1,
+  },
+  {
+    title: '별로',
+    icon: '/images/member-low.svg',
+    score: 2,
+  },
+  {
+    title: '보통',
+    icon: '/images/member-mid.svg',
+    score: 3,
+  },
+  {
+    title: '좋음',
+    icon: '/images/member-high.svg',
+    score: 4,
+  },
+  {
+    title: '최고',
+    icon: '/images/member-highest.svg',
+    score: 5,
+  },
 ];
