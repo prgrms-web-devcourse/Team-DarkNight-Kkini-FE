94f405326ed69cdecc74523509ed9da61a657121
metacode22 <alohajune22@gmail.com>
2023-03-11 15:12:06 +0900

    feat: 밥모임이 없을 때 생성하러 갈 수 있도록 버튼 생성

--- src/components/FoodParty/FoodPartyList.tsx
+++ src/components/FoodParty/FoodPartyList.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 import { FoodParty } from 'types/foodParty';
 
 import FoodPartyListItem from './FoodPartyListItem';
@@ -7,18 +7,32 @@ type FoodPartyListProps = {
   foodPartyList: FoodParty[];
   onClickViewButton: (partyId: number) => void;
   onClickReviewButton?: (partyId: number) => void;
+  onClickCreateFoodPartyButton?: () => void;
 };
 
 const FoodPartyList = ({
   foodPartyList,
   onClickViewButton,
   onClickReviewButton,
+  onClickCreateFoodPartyButton,
 }: FoodPartyListProps) => {
   return (
     <>
       {!foodPartyList.length ? (
         // To Do: 스타일링 by 승준
-        <Text>밥모임이 없어요..!</Text>
+        <Flex
+          flexDirection='column'
+          position='absolute'
+          alignItems='center'
+          gap='1rem'
+          top='50%'
+          left='50%'
+          transform='translate(-50%, -50%)'>
+          <Text>밥모임이 없어요..!</Text>
+          {onClickCreateFoodPartyButton && (
+            <Button onClick={onClickCreateFoodPartyButton}>밥모임 생성하러 가기</Button>
+          )}
+        </Flex>
       ) : (
         <Flex flexDirection='column'>
           {foodPartyList?.map((party) => (
