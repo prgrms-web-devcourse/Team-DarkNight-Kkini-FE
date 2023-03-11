5239a958983fe883d2d43a8058f92183b99cad80
sdwoo <damjang67@naver.com>
2023-03-12 02:42:00 +0900

    feat: 헤더와 마이페이지 연결

--- src/components/common/UserProfile.tsx
+++ src/components/common/UserProfile.tsx
@@ -1,8 +1,16 @@
 import { Avatar, Flex, Text } from '@chakra-ui/react';
 import { useGetUser } from 'hooks/query/useUser';
+import { useRouter } from 'next/router';
 
 const UserProfile = () => {
   const { data } = useGetUser();
+  const router = useRouter();
+
+  const handleClickUserIcon = () => {
+    if (data && data.id) {
+      router.push(`/user/${data.id}`);
+    }
+  };
 
   return (
     <Flex alignItems='center'>
@@ -10,7 +18,13 @@ const UserProfile = () => {
         <Text color='primary'>맛에 진심인, </Text>
         <Text marginLeft='0.25rem'>{data?.nickname}</Text>
       </Flex>
-      <Avatar src={data?.profileImgUrl} width='35' height='35' marginLeft='0.7rem' />
+      <Avatar
+        src={data?.profileImgUrl}
+        width='35'
+        height='35'
+        marginLeft='0.7rem'
+        onClick={handleClickUserIcon}
+      />
     </Flex>
   );
 };
