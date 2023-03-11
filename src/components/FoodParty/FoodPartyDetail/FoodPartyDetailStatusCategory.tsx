66928d678db992ac0220a6ee37db6b131e2b0eed
metacode22 <alohajune22@gmail.com>
2023-03-12 01:11:35 +0900

    refactor: FoodPartyDetailStatus 카테고리의 색깔을 판단하는 로직을 컴포넌트 밖으로 분리

--- src/components/FoodParty/FoodPartyDetail/FoodPartyDetailStatusCategory.tsx
+++ src/components/FoodParty/FoodPartyDetail/FoodPartyDetailStatusCategory.tsx
@@ -1,8 +1,23 @@
 import Category from 'components/common/Category';
 import { FoodPartyStatus } from 'types/foodParty';
 
+const FOOD_PARTY_DETAIL_STATUS_COLORS: {
+  [key: string]: string;
+  '모집 중': string;
+  '모집 종료': string;
+  '식사 완료': string;
+} = {
+  '모집 중': 'lightgreen',
+  '모집 종료': 'orange',
+  '식사 완료': 'lightgray',
+};
+
 const FoodPartyDetailStatusCategory = ({ status }: { status: FoodPartyStatus }) => {
-  return <Category foodPartyDetailStatus={status}>{status}</Category>;
+  return (
+    <Category backgroundColor={FOOD_PARTY_DETAIL_STATUS_COLORS[status]}>
+      {status}
+    </Category>
+  );
 };
 
 export default FoodPartyDetailStatusCategory;
--- src/components/common/Category.tsx
+++ src/components/common/Category.tsx
@@ -1,32 +1,19 @@
 import { Badge } from '@chakra-ui/react';
 import { ReactNode } from 'react';
-import { FoodPartyStatus } from 'types/foodParty';
 
 type CategoryProps = {
   children: ReactNode;
-  foodPartyDetailStatus?: FoodPartyStatus;
+  backgroundColor?: string;
 };
 
-const Category = ({ children, foodPartyDetailStatus }: CategoryProps) => {
-  let backgroundColor;
+const CHAKRA_BUTTON_DEFAULT_COLOR = 'gray.100';
 
-  switch (foodPartyDetailStatus) {
-    case '모집 중':
-      backgroundColor = 'lightgreen';
-      break;
-    case '모집 종료':
-      backgroundColor = 'lightyellow';
-      break;
-    case '식사 완료':
-      backgroundColor = 'lightgray';
-      break;
-    default:
-      backgroundColor = 'gray.100';
-      break;
-  }
-
+const Category = ({ children, backgroundColor }: CategoryProps) => {
   return (
-    <Badge backgroundColor={backgroundColor} borderRadius='1.5rem' padding='2px 5px'>
+    <Badge
+      backgroundColor={backgroundColor || CHAKRA_BUTTON_DEFAULT_COLOR}
+      borderRadius='1.5rem'
+      padding='2px 5px'>
       {children}
     </Badge>
   );
