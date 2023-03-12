e3938179e6d963ce6eae4e8da2fdf40fbaba6bb3
metacode22 <alohajune22@gmail.com>
2023-03-13 02:01:56 +0900

    comment: next.config.js 불필요한 주석 제거

--- next.config.js
+++ next.config.js
@@ -4,47 +4,6 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/ws/:path*',
-  //       destination: 'https://dev.kkini.site/ws/:path*',
-  //     },
-  //     {
-  //       source: '/topic/public/:path*',
-  //       destination: 'https://dev.kkini.site/topic/public/:path*',
-  //     },
-  //     {
-  //       source: '/app/chat.sendMessage/:path*',
-  //       destination: 'https://dev.kkini.site/app/chat.sendMessage/:path*',
-  //     },
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'https://dev.kkini.site/:path*',
-  //     },
-  //     {
-  //       source: '/',
-  //       destination: 'http://localhost:3000',
-  //     },
-  //     {
-  //       source: '/oauth2',
-  //       destination: 'https://dev.kkini.site',
-  //     },
-  //     {
-  //       source: '/login',
-  //       destination: 'https://dev.kkini.site',
-  //     },
-  //   ];
-  // },
-  // https://dev.kkini.site/ws?info
-  // async rewrite() {
-  //   return [
-  //     {
-  //       source: '/ws/:path*',
-  //       destination: 'https://dev.kkini.site/ws/:path*',
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = nextConfig;
