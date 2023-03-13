b37924d8b14478e7b9719828753c8a1404f003b5
sdwoo <damjang67@naver.com>
2023-03-13 18:12:44 +0900

    feat: 유저 이미지, 프로필 업데이트 함수 구현

--- src/hooks/query/useUser.ts
+++ src/hooks/query/useUser.ts
@@ -1,5 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchSpecificUser, fetchUser } from 'services/user';
+import { useToast } from '@chakra-ui/react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import {
+  fetchSpecificUser,
+  fetchUser,
+  updateUserProfile,
+  UserUpdateProfileType,
+} from 'services/user';
+import { updateUserImage } from 'services/user';
 import QUERY_KEYS from 'utils/constants/queryKeys';
 
 // custom hooks(컴포넌트에 바인딩해서 사용)
@@ -17,5 +25,33 @@ export const useGetSpecificUser = (userId: string) => {
   return useQuery({
     queryKey: [QUERY_KEYS.USER.USER_INFO, userId],
     queryFn: () => fetchSpecificUser(userId),
+  });
+};
+
+export const useUpdateUserImage = () => {
+  return useMutation({
+    mutationFn: (body: FormData) => updateUserImage(body),
+  });
+};
+
+export const useUpdateUserProfile = (userId: string) => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const toast = useToast();
+
+  return useMutation({
+    mutationFn: (body: UserUpdateProfileType) => updateUserProfile(body),
+    onSuccess: () => {
+      queryClient.invalidateQueries([QUERY_KEYS.USER.USER_INFO, userId]);
+      router.push(`/user/${userId}`);
+      toast({
+        title: '정보 수정이 완료되었습니다!',
+        description: '바뀐정보를 확인해주세요',
+        position: 'top',
+        status: 'success',
+        duration: 2000,
+        isClosable: true,
+      });
+    },
   });
 };
