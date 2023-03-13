5f7eebf97ad04ecffe1bc203d52bbf41604e3a8d
metacode22 <alohajune22@gmail.com>
2023-03-13 13:45:42 +0900

    chore: google maps api 관련 코드 삭제

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    domains: ['maps.googleapis.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'maps.googleapis.com',
-        pathname: '/_next/image/**',
-      },
-    ],
-  },
   compiler: {
     emotion: true,
   },
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,10 +12,6 @@ const Document = () => {
           src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services,clusterer&autoload=false`}
           strategy='beforeInteractive'
         />
-        <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}&libraries=places`}
-          strategy='beforeInteractive'
-        />
       </body>
     </Html>
   );
