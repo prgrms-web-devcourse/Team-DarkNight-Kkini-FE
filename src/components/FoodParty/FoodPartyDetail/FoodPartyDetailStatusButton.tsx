bb7b97ee517cf72086fd883d4d0aaf36ad9ab1e0
metacode22 <alohajune22@gmail.com>
2023-03-14 01:52:57 +0900

    design: FoodPartyDetailStatusButton 위치 변경

--- src/components/FoodParty/FoodPartyDetail/FoodPartyDetailStatusButton.tsx
+++ src/components/FoodParty/FoodPartyDetail/FoodPartyDetailStatusButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
 import { FoodPartyDetailStatusButtonText } from 'types/foodParty';
 
 type FoodPartyDetailStatusButtonProps = {
@@ -15,22 +15,20 @@ const FoodPartyDetailStatusButton = ({
   return (
     <>
       {buttonText && (
-        <Button
-          onClick={onClick}
-          disabled={isDisabled}
-          position='absolute'
-          left='50%'
-          transform='translateX(-50%)'
-          bottom='1rem'
-          height='3rem'
-          width='20rem'
-          cursor={isDisabled ? 'not-allowed' : 'pointer'}
-          opacity={isDisabled ? 0.5 : 1}
-          backgroundColor='primary'
-          color='white'
-          _hover={{ backgroundColor: 'primary' }}>
-          {buttonText}
-        </Button>
+        <Flex flex={1} height='100%' flexDirection='column-reverse' alignItems='center'>
+          <Button
+            onClick={onClick}
+            disabled={isDisabled}
+            height='3rem'
+            width='20rem'
+            cursor={isDisabled ? 'not-allowed' : 'pointer'}
+            opacity={isDisabled ? 0.5 : 1}
+            backgroundColor='primary'
+            color='white'
+            _hover={{ backgroundColor: 'primary' }}>
+            {buttonText}
+          </Button>
+        </Flex>
       )}
     </>
   );
--- src/components/FoodParty/FoodPartyDetail/FoodPartyMemberItem.tsx
+++ src/components/FoodParty/FoodPartyDetail/FoodPartyMemberItem.tsx
@@ -4,11 +4,11 @@ import { Member } from 'types/foodParty';
 
 const FoodPartyMemberItem = ({ member }: { member: Member }) => {
   return (
-    <Box position='relative' padding='0.5rem 0'>
+    <Box position='relative' padding='0.25rem 0'>
       <Flex alignItems='center' justifyContent='space-between'>
-        <Flex alignItems='center' gap='1rem'>
-          <Avatar src={member.profileImgUrl} />
-          <Text>{member.nickname}</Text>
+        <Flex alignItems='center' gap='0.5rem'>
+          <Avatar src={member.profileImgUrl} size='sm' />
+          <Text fontSize='14px'>{member.nickname}</Text>
         </Flex>
         {member.crewMemberRole === 'LEADER' && (
           <Flex border='1px solid' borderRadius='50%' padding='0.25rem'>
